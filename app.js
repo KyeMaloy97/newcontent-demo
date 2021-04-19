6c48ce0e22d99e6eb8facb219414b5de1203ff89
Kye Maloy <kye.maloy@ibm.com>
2021-04-19 12:03:47 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+const server = http.createServer(app);
+
+app.get('/', function(req,res){
+        res.sendFile(__dirname + '/index.html');
+});
+
+
+server.listen(3000, function(){
+        console.log("Server is listening on port: 3000");
+});
